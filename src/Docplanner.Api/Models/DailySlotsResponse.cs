d24c0f7c5eae5d7e6ca8f99357908d5a1ad55829
JOSEP PORCAR NADAL <jjppnn@hotmail.com>
2025-04-20 18:03:25 +0200

    feat: dralia-1: Slot Api (#1)
    * feat: dralia-1: Initial commit. Slot Api
    
    * feat: dralia-1: Unit Tests, Integration Tests and CI

---
+++ src/Docplanner.Api/Models/DailySlotsResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace Docplanner.Api.Models
+{
+    public class DailySlotsResponse
+    {
+        public DateOnly Date { get; set; }
+        public string? DayOfWeek { get; set; }
+        public List<SlotResponse> Slots { get; set; } = new();
+        public WorkPeriodResponse? WorkPeriod { get; set; }
+    }
+}
---
+++ src/Docplanner.Api/Models/WeeklySlotsResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace Docplanner.Api.Models
+{
+
+    public class WeeklySlotsResponse
+    {
+        public List<DailySlotsResponse> Days { get; set; } = new();
+        public FacilityResponse? Facility { get; set; }
+        public int SlotDurationMinutes { get; set; }
+    }
+}
