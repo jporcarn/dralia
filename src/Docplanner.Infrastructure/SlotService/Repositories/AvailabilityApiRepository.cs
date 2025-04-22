8ae3461a4f56e1c9df5f8ae9bcf369bcab862a94
JOSEP PORCAR NADAL <jjppnn@hotmail.com>
2025-04-22 22:57:15 +0200

    feat: dralia-6: Add the health check endpoints (#10)
    * feat: dralia-6: Add the health check endpoints
    
    * fix: dralia-6: Fix workflows
    ci-cd: Build, Run Unit Tests, Tag and Deploy latest tag only when merging to main
    ci: Build and Run Unit Test when a PR to main

--- src/Docplanner.Infrastructure/SlotService/Repositories/AvailabilityApiRepository.cs
+++ src/Docplanner.Infrastructure/SlotService/Repositories/AvailabilityApiRepository.cs
@@ -2,11 +2,13 @@
 using Docplanner.Api.Models;
 using Docplanner.Application.Interfaces.Repositories;
 using Docplanner.Infrastructure.SlotService.Models;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System.Collections.ObjectModel;
 using System.Net.Http.Json;
 
 namespace Docplanner.Infrastructure.SlotService.Repositories
 {
-    public class AvailabilityApiRepository : IAvailabilityRepository
+    public class AvailabilityApiRepository : IAvailabilityRepository, IHealthCheck
     {
         private readonly HttpClient _httpClient;
         private readonly IMapper _mapper;
@@ -15,6 +17,51 @@ namespace Docplanner.Infrastructure.SlotService.Repositories
         {
             this._httpClient = httpClient;
             this._mapper = mapper;
+        }
+
+        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                // Check if the Availability API is reachable
+
+                var today = DateTime.Today;
+
+                // Calculate the Monday of the current week
+                var mondayOfTodaysWeek = today.AddDays(-(int)today.DayOfWeek + (int)DayOfWeek.Monday);
+                var mondayDateOnly = DateOnly.FromDateTime(mondayOfTodaysWeek);
+
+                var response = await this.GetWeeklyAvailabilityAsync(mondayDateOnly);
+                return HealthCheckResult.Healthy("Availability API is reachable.");
+            }
+            catch (HttpRequestException ex)
+            {
+                var additionalData = new Dictionary<string, object>
+                {
+                    { "StatusCode", ex.StatusCode?.ToString() ?? "Unknown" },
+                    { "RequestUri", _httpClient.BaseAddress?.ToString() ?? "Unknown" }
+                };
+
+                switch (ex.StatusCode)
+                {
+                    case System.Net.HttpStatusCode.Unauthorized:
+                        additionalData.Add("Suggestion", "Credentials used to call the Availability API might be wrong or expired.");
+                        return HealthCheckResult.Unhealthy("Unauthorized access to Availability API.", ex, additionalData);
+
+                    case System.Net.HttpStatusCode.NotFound:
+                        additionalData.Add("Suggestion", "The URL of the Availability API might be wrong or temporary unabailable.");
+                        return HealthCheckResult.Unhealthy("Availability API not found.", ex, additionalData);
+
+                    default:
+                        break;
+                }
+
+                return HealthCheckResult.Unhealthy(ex.Message, ex);
+            }
+            catch (Exception ex)
+            {
+                return HealthCheckResult.Unhealthy("Availability API health check failed.", ex);
+            }
         }
 
         public async Task<WeeklySlots> GetWeeklyAvailabilityAsync(DateOnly mondayDate)
